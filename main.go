108ec77dac07d6ae7c51b59591e7f49f943506b4
Sankalp Bhatt <sankalp.bhatt@jabong.com>
2014-12-16 18:15:39 +0530

    Building Blocks for SRT fixer constructed

--- main.go
+++ main.go
@@ -1,137 +1,34 @@
 package main
 
 import (
-	"fmt"
-	//"gopkg.in/mgo.v2"
-	//"gopkg.in/mgo.v2/bson"
-	//"io"
-	//"io/ioutil"
-	//"log"
-	"os"
-	//"regexp"
-	"strings"
+	//"fmt"
+	//"os"
+	//"strings"
 	"srtfixer/srt"
-	//"time"
+	"flag"
 )
-
-type Person struct {
-	Name  string
-	Voila int
-}
 
 type Config struct {
 	File       string
-	Start      string
-	End        string
-	Lag        string
-	IsPositive string
+	Lag        int64
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+var gConfig Config
+
+func init() {
+	filePtr := flag.String("file", "", "a string")
+	secPtr  := flag.Int("sec", 0, "a string")
+	msecPtr := flag.Int("msec", 0, "a string")
+	flag.Parse()
+	lag := int64(*msecPtr * 1000000  + *secPtr * 1000000000)
+	gConfig = Config {
+		File: *filePtr,
+		Lag: lag,
 	}
-}
-
-func prepareArgs(args []string) Config {
-
-	configStruct := Config{}
-
-	for _, element := range args {
-		splitElement := strings.Split(element, "=")
-		//fmt.Println(splitElement)
-
-		if strings.Contains(splitElement[0], "--file") {
-			configStruct.File = splitElement[1]
-		} else if strings.Contains(splitElement[0], "--start") {
-			configStruct.Start = splitElement[1]
-		} else if strings.Contains(splitElement[0], "--end") {
-			configStruct.End = splitElement[1]
-		} else {
-			//what is this.
-		}
-	}
-
-	return configStruct
-}
-
-func (c Config) isValid() bool {
-
-	returnVar := true
-	//check if the supplied config is valid.
-
-	//check if supplied file is valid
-	if _, err := os.Stat(c.File); err != nil {
-		returnVar = false
-	}
-	return returnVar
 }
 
 func main() {
-
-
-	da,_ := srt.FetchFileContents("/home/jackson/test2.srt")
-	de := srt.ParseData(da)
-	du:= srt.AddLagToTime(de, -1000000000)
-	srt.WriteData(du, "/home/jackson/test.srt")
-	fmt.Println("")
-	return
-/*
-	csvfile, err := os.Open("/home/jackson/test.srt")
-
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer csvfile.Close()
-
-
-
-	dat, err := ioutil.ReadFile("/home/jackson/test.srt")
-
-	dat1 := strings.Replace(string(dat), "\r", "", -1)
-	//contents := RegSplit(dat1, "\n\n")
-	for _, element := range contents {
-		fmt.Println(element)
-		fmt.Println("sdsdsdsdsdsdds")
-	}
-	return
-	fmt.Printf("%+v", contents)
-	return
-
-	cc := prepareArgs(os.Args).isValid()
-	fmt.Printf("%+v", cc)
-	return
-	for index, element := range os.Args {
-		fmt.Println(index)
-		fmt.Println(element)
-	}
-	//argsWithProg := os.Args
-	//argsWithoutProg := os.Args[1:]
-	//a := os.Args[1]
-	//fmt.Println(argsWithProg)
-	//fmt.Println(argsWithoutProg)
-	//fmt.Println(a)
-
-	return
-
-	session, err := mgo.Dial("localhost")
-
-	if err != nil {
-		fmt.Println("Connection failed")
-	}
-
-	result := Person{}
-	testData := session.DB("mydb").C("testData")
-	testData.Insert()
-	err1 := session.DB("mydb").C("testData").Find(bson.M{}).One(&result)
-
-	if err1 != nil {
-		log.Fatal(err1)
-	}
-
-	fmt.Printf("%+v", result)
-*/
+    data, _ := srt.ReadFile(gConfig.File)
+	data = srt.AddLagToTime(data, gConfig.Lag)
+    srt.WriteFile(data, gConfig.File)
 }
--- srt/strreader.go
+++ srt/strreader.go
@@ -1,21 +1,31 @@
 package srt
 
+//import the required Packages.
 import (
-
 	"io/ioutil"
-	"strings"
-	//"srtfixer/utility"
-	//"fmt"
-	"strconv"
-	"time"
 	"os"
-	//"fmt"
-
-	//"path/filepath"
-	"fmt"
+	"strconv"
+	"strings"
+	"time"
 )
 
-
+// Each portion of the SRT such as:
+//
+// --------- Block 1 -----------------
+// 324
+// 01:58:11,142 --> 01:58:12,940
+// What are you doing?
+// ---------- Block 2 ---------------
+// 325
+// 01:58:49,139 --> 01:58:50,812
+// Shut up!
+//
+// are assumed to be blocks and SRT consists of several of such blocks.
+// Further, each block consists of following entities:
+// a:) Serial number [starting from 1, mainly]
+// b:) Duration for which the subtitle needs to be shown
+//     [Start and End time are seperated via "-->" characters]
+// c:) Contents to be displayed during this period.
 type Block struct {
 	Serial  int
 	Start   time.Time
@@ -23,92 +33,123 @@ type Block struct {
 	Content string
 }
 
-//Fetch the contents of the specified file.
-func FetchFileContents(filePath string) (string, error) {
-	dat, err := ioutil.ReadFile(filePath)
-
-	if err != nil {
-		return "", err
-	}
-	return string(dat), nil
-}
-
-func WriteData(dataArr []Block, filepath string) bool{
-
-	csvfile, er := os.OpenFile(filepath, os.O_RDWR, 0777)
-	fmt.Printf("%v", er)
-	dataStr := ""
-	for _,e := range dataArr {
-		dataStr = dataStr + strconv.Itoa(e.Serial) + "\r\n"
-		milliSecString := strconv.Itoa(e.Start.Nanosecond()/1000000)
-		dataStr = dataStr + e.Start.Format("15:04:05") + "," + milliSecString
-				+ " --> " + e.End.Format("15:04:05") + "\r\n"
-		dataStr = dataStr + e.Content + "\r\n\r\n"
-	}
-	_ , er = csvfile.Write([]byte(dataStr))
-	fmt.Printf("%v", er)
-	defer csvfile.Close()
-	return true;
-}
-
-//Parse Supplied data
-//@todo: work on datetime go functions
-func ParseData(content string) []Block {
-	//remove carriage return chars first
-	content = strings.Replace(content, "\r", "", -1)
-	//Split into blocks containing three sets
-	contentBlocks := strings.Split(content, "\n\n")
-
-	//declare a Slice to store Blocks.
-	var mainC []Block
-	for _, e := range contentBlocks {
-		block := strings.SplitN(e, "\n", 3)
-        if len(block) >= 3 {
-			serial, _ := strconv.Atoi(block[0])
-			dateArr := strings.Split(block[1], " --> ")
-			//Extend our container array
-			mainCNew := make([]Block, len(mainC)+1)
-			for i := range mainC {
-				mainCNew[i] = mainC[i]
-			}
-			mainC = mainCNew
-
-			startDate := strings.Split(dateArr[0],",")
-			startDateTime,_ := time.Parse("15:04:05", startDate[0])
-
-			//add nanoseconds to thi s tim
-			startDateTime.Nanosecond()
-
-			intMicroSeconds,_ := strconv.Atoi(startDate[1])
-			startDateTime = startDateTime.Add(time.Duration(intMicroSeconds) * 1000000)
-
-			endDate := strings.Split(dateArr[1],",")
-			endDateTime,_ := time.Parse("15:04:05", endDate[0])
-			intMicroSeconds,_ = strconv.Atoi(endDate[1])
-			endDateTime = endDateTime.Add(time.Duration(intMicroSeconds) * 1000000)
-
-			//put the block in it.
-			mainC[len(mainC)-1] = Block{
-				Serial  : serial,
-				Start   : startDateTime,
-				End     : endDateTime,
-				Content : block[2],
-			}
-		}
-		//fmt.Printf("%v", mainC)
-	}
-	return mainC
+//Add time lag.
+func (b *Block) AddTimeLag(lag time.Duration) *Block {
+	b.Start = b.Start.Add(lag)
+	b.End = b.End.Add(lag)
+	return b
 }
 
 //prepare data based on lag
-func AddLagToTime(originalData []Block, lag time.Duration ) []Block {
+func AddLagToTime(originalData []Block, lag int64) []Block {
 
-	for i,e := range originalData {
-		originalData[i].Start = e.Start.Add(lag)
-		originalData[i].End   = e.End.Add(lag)
+	lagTime := time.Duration(lag)
+	for i, _ := range originalData {
+		originalData[i].AddTimeLag(lagTime)
 	}
 	return originalData
 }
 
+//Read the file specified by the path.
+//Break the SRT file contents into chunks of []Block
+func ReadFile(filePath string) ([]Block, error) {
+	fileContents, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return []Block{}, err
+	}
+	blocks := constructBlocks(string(fileContents))
+	return blocks, nil
+}
 
+//Write the supplied []Block array to specified file.
+func WriteFile(blocks []Block, filepath string) bool {
+	//open the file in read-write mode.
+	f, err := os.OpenFile(filepath, os.O_RDWR, 0644)
+	if err != nil {
+		//error found in opening file.
+		return false
+	}
+	//Note: defer is called after function returns.
+	defer f.Close()
 
+	dataStr := blocks2String(blocks)
+	_, err   = f.Write([]byte(dataStr))
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+//Extend the []Block array
+func extendArray(arr []Block) []Block {
+	arrNew := make([]Block, len(arr)+1)
+	for i := range arr {
+		arrNew[i] = arr[i]
+	}
+	arr = arrNew
+	return arr
+}
+
+//Extract time out of the supplied string
+//Assumed string to be of format:
+// 01:58:11,142 --> 01:58:12,940
+func extractTime(rawTime string, tType string) time.Time {
+	index := 0
+	if tType != "START" {
+		index = 1
+	}
+	aTime := strings.Split(rawTime, " --> ")
+	aTime = strings.Split(aTime[index], ",")
+
+	parsedTime, _ := time.Parse("15:04:05", aTime[0])
+	//Now we have the time.Time in parsedTime, but we need to add
+	//microseconds to it as well.
+	microSeconds, _ := strconv.Atoi(aTime[1])
+	nanoSeconds := time.Duration(microSeconds) * 1000000
+	parsedTime = parsedTime.Add(nanoSeconds)
+	return parsedTime
+}
+
+//Converts the supplied []Block array to string format.
+func blocks2String(blocks []Block) string {
+	dataStr := ""
+	for _, e := range blocks {
+		dataStr += strconv.Itoa(e.Serial) + "\r\n"
+		milliSecString := strconv.Itoa(e.Start.Nanosecond() / 1000000)
+		milliSecStringE := strconv.Itoa(e.End.Nanosecond() / 1000000)
+		dataStr = dataStr + e.Start.Format("15:04:05") + "," + milliSecString +
+			" --> " + e.End.Format("15:04:05") + "," + milliSecStringE + "\r\n"
+		dataStr = dataStr + e.Content + "\r\n\r\n"
+	}
+	return dataStr
+}
+
+//Construct the []Block array based on the string data supplied.
+func constructBlocks(fileContent string) []Block {
+	//Remove the carriage return \r characters from content.
+	fileContent = strings.Replace(fileContent, "\r", "", -1)
+	//Split into blocks containing three sets
+	rawCBlocks := strings.Split(fileContent, "\n\n")
+	//declare a Slice to store Blocks.
+	var blocks []Block
+	for _, e := range rawCBlocks {
+		block := strings.SplitN(e, "\n", 3)
+		if len(block) >= 3 {
+			//Extend our container array
+			blocks = extendArray(blocks)
+			//Extract Serial, Start-End date and Content
+			SNo, _ := strconv.Atoi(block[0])
+			StartTime := extractTime(block[1], "START")
+			EndTime := extractTime(block[1], "END")
+			Content := block[2]
+			//put the block in it.
+			blocks[len(blocks)-1] = Block{
+				Serial:  SNo,
+				Start:   StartTime,
+				End:     EndTime,
+				Content: Content,
+			}
+		}
+	}
+	return blocks
+}
